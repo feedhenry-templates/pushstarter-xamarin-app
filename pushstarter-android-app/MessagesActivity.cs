c156986720583e19d45e5a9e7fb3dd74eed139f5
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-03-02 11:40:19 +0100

    initial version android

---
+++ pushstarter-android-app/MessagesActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+
+namespace pushstarter_android_app
+{
+    [Activity(Label = "@string/notifications", MainLauncher = true, Theme = "@style/MyTheme.Base")]
+    public class MessagesActivity : AppCompatActivity
+    {
+        private PushStarterApplication _application;
+        private ListView _listview;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.messages);
+
+            _application = (PushStarterApplication) Application;
+            var emptyView = FindViewById(Resource.Id.empty);
+            _listview = FindViewById<ListView>(Resource.Id.messages);
+            _listview.EmptyView = emptyView;
+        }
+
+        private void AddNewMessage(Bundle bundle)
+        {
+            _application.Messages.Add(bundle.GetString("alert"));
+            DisplayMessages();
+        }
+
+        private void DisplayMessages()
+        {
+            var adapter = new ArrayAdapter(ApplicationContext, Resource.Layout.message_item, _application.Messages);
+            _listview.Adapter = adapter;
+        }
+    }
+}
---
+++ pushstarter-android-app/PushStarterApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Runtime;
+
+namespace pushstarter_android_app
+{
+    [Application(Name = "pushstarter_android_app.PushStarterApplication", Icon = "@mipmap/ic_launcher", Label = "@string/app_name", Theme = "@style/MyTheme.Base", AllowBackup = false)]
+    public class PushStarterApplication : Application
+    {
+        public List<string> Messages { get; } = new List<string>();
+
+        public PushStarterApplication(IntPtr handle, JniHandleOwnership transfer)
+            : base(handle,transfer)
+        {
+        }
+    }
+}
